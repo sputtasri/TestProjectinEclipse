e4ffdc29d8125b9bac8567444a62a0ebc39699e7
sputtasri <srilathaputta50@gmail.com>
2023-02-05 23:05:10 -0500

    test

---
+++ TestProjectinEclipse/src/com/test/SampleGitTest.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.test;
+
+/**
+ * @author rmetuku
+ *
+ */
+public class SampleGitTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
